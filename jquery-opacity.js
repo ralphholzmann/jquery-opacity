605a2492a9a90020679c2b833e075a3bc5080fdd
Ralph Holzmann <ralphholzmann@gmail.com>
2011-04-15 19:06:45 -0500

    Complete rewrite.

--- jquery-opacity.js
+++ jquery-opacity.js
@@ -1,110 +1,76 @@
 (function($){
+  
+  var _css, _animate, hijackCallback, hijackArgs;
 
-    if ( $.browser.msie ) {
+    if ( ! $.support.opacity ) {
 
-        // For detecting is opacity is already set
-        var alphaRegex = /^alpha\(opacity=(\d+)\)$/i;
+      // Callback wrapper that removes filter attribute
+      hijackCallback = function( callback ) {
+        return function () {
+          if ( /^(0|1)$/.test( _css.call( $(this), 'opacity' ))) {
+            this.style.removeAttribute('filter');
+          }
+          (callback || $.noop).call(this);
+        }
+      }
+      
+      // Loops through arguments, hijacking callback if it
+      // exists. If a callback doesn't exist, one is added.
+      hijackArgs = function( args ) {
+        var found;
 
-        // Handle fadeIn and fadeOut methods
-        $.each(['fadeIn', 'fadeOut'], function (i, method) {
-            var _fn = $.fn[method];
-            $.fn[method] = function (easing, callback) {
-                if ((!this[0].style.filter || alphaRegex.test(this[0].style.filter))) {
-                    if ($.isFunction(easing)) {
-                        callback = easing;
-                        easing = 'normal';
-                    }
-                    var _callback = callback;
-                    callback = function () {
-                        this.style.removeAttribute('filter');
-                        (_callback || $.noop).call(this);
-                    }
-                }
-                return _fn.call(this, easing, callback);
-            }
+        args = $.map( args, function( arg ) {
+          if ( $.isFunction( arg )) {
+            found = true;
+            arg = hijackCallback( arg );
+          }
+          return arg;
         });
-        
-        // Handle fadeTo method when opacity is set to 1
-        var _fadeTo = $.fn.fadeTo;
-        $.fn.fadeTo = function (duration, opacity, callback) {
-            if ( (!this[0].style.filter || alphaRegex.test(this[0].style.filter)) && parseInt(opacity) == 1) {
-                var _callback = callback;
-                callback = function () {
-                    this.style.removeAttribute('filter');
-                    (_callback || $.noop).call(this);
-                }
-            }
-            return _fadeTo.call(this, duration, opacity, callback);
+
+        if ( ! found ) {
+          args.push( hijackCallback() );
         }
-        
-        // Handle animate method when it contains opacity and that opacity
-        // is set to 1
-        var _animate = $.fn.animate;
-        $.fn.animate = function () {
-            var args = Array.prototype.slice.call(arguments);
-            if ( 'opacity' in args[0] && (!this[0].style.filter || alphaRegex.test(this[0].style.filter)) && parseInt(args[0].opacity) == 1) {
-                var i, fn, found = false;
-                for (i = 1; i < args.length; i++) {
-                    if ($.isFunction(args[i])) {
-                        found = true;
-                        break;
-                    }
-                }
-                // If callback is a function
-                if (found) {
-                    fn = args[i];
-                    args[i] = function () {
-                        this.style.removeAttribute('filter');
-                        (fn || $.noop).call(this);
-                    }
-                    // If callback is 'complete' in options object
-                } else if (typeof args[1] == 'object') {
-                    fn = args[1].complete;
-                    args[1].complete = function () {
-                        this.style.removeAttribute('filter');
-                        (fn || $.noop).call(this);
-                    };
-                    // Else add our own callback
-                } else {
-                    args.push(function () {
-                        this.style.removeAttribute('filter');
-                    });
-                }
-            }
-            return _animate.apply(this, args);
+
+        return args;
+      }
+
+          
+      // Handle css method
+      _css = $.fn.css;
+      $.fn.css = function () {
+
+        // Exit early if empty
+        if ( ! this.length ) return this;
+
+        // Apply styles
+        _css.apply( this, arguments );
+
+        // Remove filter if opacity is 0 or 1
+        if ( arguments[0] == 'opacity' || 'opacity' in arguments[0] ) {
+          this.each( hijackCallback() );
+        } 
+
+        return this;
+      };
+
+      // Handle animate method when it contains opacity
+      _animate = $.fn.animate;
+      $.fn.animate = function () {
+        var args = Array.prototype.slice.call( arguments );
+
+        // Exit early if empty
+        if ( ! this.length ) return this;
+
+        if ( 'opacity' in args[0] ) {
+          if ( $.isPlainObject( args[1] ) ) {
+            args[1].complete = hijackCallback( args[1].complete );
+          } else {
+            args = hijackArgs( args );
+          }
         }
-        
-        // Handle css method
-        var _css = $.fn.css;
-        $.fn.css = function () {
+        return _animate.apply( this, args );
+      }
+      
+  }
 
-            if (!this.length) return this;
-
-            var args = arguments,
-                result = _css.apply(this, arguments);
-
-            if  (
-                    (
-                            ! this[0].style.filter 
-                        ||  alphaRegex.test(this[0].style.filter )
-                    ) // Filter attribute exists on element
-                &&  (
-                        (
-                                typeof args[0] == 'object'
-                            &&  'opacity' in args[0] 
-                            &&  parseInt(args[0].opacity) == 1
-                        ) // Trying to change opacity to 1
-                ||      (
-                                args[0] == 'opacity' 
-                            &&  parseInt(args[1]) == 1
-                        ) // Trying to change opacity to 1
-                    )
-                ) {
-                this[0].style.removeAttribute('filter');
-            }
-            
-            return result;
-        };
-    }
-
-})(jQuery)
+  })( jQuery )
